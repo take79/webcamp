7edddb0271afb385b317812df732e47ca3cb3698
takenoko <tk.hrsk79@gmail.com>
2016-11-22 14:02:05 +0900

    git init

---
+++ sample_blog/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create'
+  get '/posts' => 'posts#index'
+  get '/posts/:id' => 'posts#show', as: 'post'
+  get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
+  delete 'posts/:id' => 'posts#destroy', as: 'destroy_post'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
