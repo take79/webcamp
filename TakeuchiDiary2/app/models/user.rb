6bef7e0fbce17aa479a30975c5498be0c06f569a
takenoko <tk.hrsk79@gmail.com>
2016-12-06 18:58:13 +0900

    modify user model

--- TakeuchiDiary2/app/models/user.rb
+++ TakeuchiDiary2/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
-  has_many :likes
+  has_many :likes, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
