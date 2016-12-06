88c26085310448e26d8b4022702cfc9686ca3a5a
take79 <tk.hrsk79@gmail.com>
2016-12-06 09:54:59 +0000

    まとまったディレクトリでgitするんじゃなかった

--- TakeuchiDiary2/db/schema.rb
+++ TakeuchiDiary2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206081542) do
+ActiveRecord::Schema.define(version: 20161206091528) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20161206081542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
